bdcaae06d7573da960c3eb6bff4002c88a9fb6eb
gesinn-it <github@gesinn.it>
2021-01-25 13:24:56 +0100

    install-mediawiki.sh execute commands as sudo

--- build/travis/install-mediawiki.sh
+++ build/travis/install-mediawiki.sh
@@ -3,17 +3,19 @@ set -ex
 
 cd /var/www
 
-wget https://github.com/wikimedia/mediawiki/archive/$MW.tar.gz
-tar -zxf $MW.tar.gz
-mv mediawiki-$MW mw
+sudo wget https://github.com/wikimedia/mediawiki/archive/$MW.tar.gz
+sudo tar -zxf $MW.tar.gz
+sudo mv mediawiki-$MW mw
+
+ls -la /var/www
 
 cd mw
 
 ## MW 1.25 requires Psr\Logger
 if [ -f composer.json ]
 then
-  composer install
+  sudo composer install
 fi
 
-mysql -e 'create database traviswiki;'
-php maintenance/install.php --dbtype mysql --dbuser root --dbname traviswiki --dbpath $(pwd) --pass AdminPassword TravisWiki admin --scriptpath /TravisWiki
+sudo mysql -e 'create database traviswiki;'
+sudo php maintenance/install.php --dbtype mysql --dbuser root --dbname traviswiki --dbpath $(pwd) --pass AdminPassword TravisWiki admin --scriptpath /TravisWiki
