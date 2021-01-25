1bdeafcf15bb34a08a138d92f091224f7fd2c4f9
gesinn-it <github@gesinn.it>
2021-01-25 21:36:27 +0100

    next attempt to fix Apache confs, DB setup

--- build/travis/install-mediawiki.sh
+++ build/travis/install-mediawiki.sh
@@ -21,4 +21,10 @@ then
 fi
 
 sudo /usr/bin/mysql -e 'create database traviswiki;'
-sudo /home/travis/.phpenv/shims/php maintenance/install.php --dbtype mysql --dbuser root --dbname traviswiki --dbpath $(pwd) --pass AdminPassword TravisWiki admin --scriptpath /TravisWiki
+sudo mysql -e "CREATE USER IF NOT EXISTS 'traviswiki'@'%';"
+sudo mysql -e "ALTER USER 'traviswiki'@'%' IDENTIFIED BY 'traviswiki';"
+sudo mysql -e "CREATE USER IF NOT EXISTS 'traviswiki'@'localhost';"
+sudo mysql -e "ALTER USER 'traviswiki'@'localhost' IDENTIFIED BY 'traviswiki';"
+sudo mysql -e "grant select, insert, update, delete, alter, drop, create, create temporary tables, index on \`traviswiki\`.* to 'traviswiki'@'%' WITH GRANT OPTION"
+sudo mysql -e "grant select, insert, update, delete, alter, drop, create, create temporary tables, index on \`traviswiki\`.* to 'traviswiki'@'localhost' WITH GRANT OPTION"
+sudo /home/travis/.phpenv/shims/php maintenance/install.php --dbtype mysql --dbuser traviswiki --dbpass traviswiki --dbname traviswiki --pass AdminPassword TravisWiki Admin
