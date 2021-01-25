93c2917716a0917ea23bc91a15e201cd9251808b
gesinn-it <github@gesinn.it>
2021-01-25 16:39:22 +0100

    attempt to run php

--- build/travis/install-mediawiki.sh
+++ build/travis/install-mediawiki.sh
@@ -12,11 +12,14 @@ ls -la /var/www
 cd mw
 
 which composer
+which mysql
+which php
 
 if [ -f composer.json ]
 then
-  sudo /home/travis/.phpenv/shims/composer install
+  # sudo /home/travis/.phpenv/shims/composer install
+  composer install
 fi
 
-sudo mysql -e 'create database traviswiki;'
-sudo php maintenance/install.php --dbtype mysql --dbuser root --dbname traviswiki --dbpath $(pwd) --pass AdminPassword TravisWiki admin --scriptpath /TravisWiki
+mysql -e 'create database traviswiki;'
+php maintenance/install.php --dbtype mysql --dbuser root --dbname traviswiki --dbpath $(pwd) --pass AdminPassword TravisWiki admin --scriptpath /TravisWiki
