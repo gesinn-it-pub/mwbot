176bb693a302228f8f0fea05310ce2e2e93c3ebe
gesinn-it <github@gesinn.it>
2021-01-29 09:00:32 +0100

    set permissions for MW directory

--- build/travis/install-mediawiki.sh
+++ build/travis/install-mediawiki.sh
@@ -7,8 +7,6 @@ sudo wget https://github.com/wikimedia/mediawiki/archive/$MW.tar.gz
 sudo tar -zxf $MW.tar.gz
 sudo mv mediawiki-$MW mw
 
-ls -la /var/www
-
 cd mw
 
 which composer
@@ -29,5 +27,10 @@ sudo mysql -e "ALTER USER 'traviswiki'@'localhost' IDENTIFIED BY 'traviswiki';"
 sudo mysql -e "grant select, insert, update, delete, alter, drop, create, create temporary tables, index on \`traviswiki\`.* to 'traviswiki'@'%' WITH GRANT OPTION"
 sudo mysql -e "grant select, insert, update, delete, alter, drop, create, create temporary tables, index on \`traviswiki\`.* to 'traviswiki'@'localhost' WITH GRANT OPTION"
 sudo /home/travis/.phpenv/shims/php maintenance/install.php --dbtype mysql --dbuser traviswiki --dbpass traviswiki --dbname traviswiki --pass AdminPassword TravisWiki Admin
+
+ls -la /var/www/mw
+sudo /usr/bin/find /var/www/mw -type d -exec chmod 775 {} \;
+sudo /usr/bin/find /var/www/mw -type f -exec chmod 644 {} \;
+
 sudo /usr/bin/echo '$wgEnableUploads=true;' >> LocalSettings.php
 sudo /usr/bin/cat LocalSettings.php
